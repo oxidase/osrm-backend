f5e617c9142d0829af9c206a8680bd5ddf931651
Michael Krasnyk <michael.krasnyk@gmail.com>
2017-04-24 20:06:33 +0200

    use generation_array

--- include/util/search_heap.hpp
+++ include/util/search_heap.hpp
@@ -1,16 +1,75 @@
 #ifndef OSRM_UTIL_SEARCH_HEAP_HPP
 #define OSRM_UTIL_SEARCH_HEAP_HPP
 
+#include "util/typedefs.hpp"
+
 #include <boost/heap/d_ary_heap.hpp>
 
 #include <deque>
 #include <type_traits>
 #include <unordered_map>
 
+
 namespace osrm
 {
 namespace util
 {
+
+template <typename Key, typename Value> class generation_array
+{
+    using counter_t = std::uint16_t;
+
+public:
+    explicit generation_array()
+        : generation(0)
+    {
+    }
+
+    void rehash(std::size_t size)
+    {
+        generation = 1;
+        positions.resize(size);
+        generations.resize(size, 0);
+    }
+
+    auto insert(const std::pair<Key, Value> &data)
+    {
+        BOOST_ASSERT(data.first < generations.size());
+        positions[data.first] = data.second;
+        generations[data.first] = generation;
+    }
+
+    Value find(const Key node) const
+    {
+        if (generations[node] < generation)
+        {
+            return end();
+        }
+        return positions[node];
+    }
+
+    Value end() const
+    {
+        return nullptr;
+    }
+
+    void clear()
+    {
+        generation++;
+        // if generation overflows we end up at 0 again and need to clear the vector
+        if (generation == 0)
+        {
+            generation = 1;
+            std::fill(generations.begin(), generations.end(), 0);
+        }
+    }
+
+  private:
+    counter_t generation;
+    std::vector<counter_t> generations;
+    std::vector<Value> positions;
+};
+
 
 template <typename NodeID, typename Weight, typename Data = void> struct SearchHeap
 {
@@ -21,7 +80,7 @@ template <typename NodeID, typename Weight, typename Data = void> struct SearchH
     struct NodeData;
     using DataContainer = std::deque<NodeData>;
     using DataReference = typename std::add_pointer<typename DataContainer::reference>::type;
-    using DataIndex = std::unordered_map<NodeID, DataReference>;
+    using DataIndex = generation_array<NodeID, DataReference>;
     using HeapData = std::pair<WeightType, DataReference>;
     using HeapContainer = boost::heap::d_ary_heap<HeapData,
                                                   boost::heap::arity<4>,
@@ -41,7 +100,7 @@ template <typename NodeID, typename Weight, typename Data = void> struct SearchH
     DataIndex index;
     HeapContainer heap;
 
-    SearchHeap(size_t) { index.rehash(1000); }
+    SearchHeap(size_t size) { index.rehash(size); }
 
     void Clear()
     {
@@ -65,25 +124,25 @@ template <typename NodeID, typename Weight, typename Data = void> struct SearchH
     DataType const &GetData(NodeID node) const
     {
         BOOST_ASSERT(WasInserted(node));
-        return index.find(node)->second->data;
+        return index.find(node)->data;
     }
 
     DataType &GetData(NodeID node)
     {
         BOOST_ASSERT(WasInserted(node));
-        return index.find(node)->second->data;
+        return index.find(node)->data;
     }
 
     WeightType const &GetKey(NodeID node) const
     {
         BOOST_ASSERT(WasInserted(node));
-        return index.find(node)->second->weight;
+        return index.find(node)->weight;
     }
 
     bool WasRemoved(const NodeID node) const
     {
         BOOST_ASSERT(WasInserted(node));
-        return index.find(node)->second->handle == HeapHandle();
+        return index.find(node)->handle == HeapHandle();
     }
 
     bool WasInserted(const NodeID node) const { return index.find(node) != index.end(); }
@@ -120,7 +179,7 @@ template <typename NodeID, typename Weight, typename Data = void> struct SearchH
     void DecreaseKey(NodeID node, WeightType weight, DataType node_data)
     {
         BOOST_ASSERT(WasInserted(node));
-        auto reference = index.find(node)->second;
+        auto reference = index.find(node);
         reference->weight = weight;
         reference->data = node_data;
         heap.increase(reference->handle, std::make_pair(weight, reference));
